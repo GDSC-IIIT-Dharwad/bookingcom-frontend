f62fb636c4a28abf2399658d90da7c91bec050a2
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2022-11-09 01:14:49 +0530

    Browse By Property

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-
+import BrowseByProperty from './pages/home/BrowseByProperty'
 import Home from './pages/home/Home'
 import Next_Trip from './pages/home/next_trip/Next_trip'
-
 import './index.css'
 
 function App() {
@@ -11,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Home />
+      <BrowseByProperty />
       <Next_Trip/>
       <Footer />
     </>
