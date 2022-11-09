c6373220515bf3e5e790012d272fe5f181515987
Harsh Raj <rajharsh1703@gmail.com>
2022-11-09 16:16:00 +0530

    added offer section in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import Explorearround from './explorearround'
-import Slider from './Slider'
-
+import HomeCss from './home.css';
 function Home() {
-  return <div>
-    <Slider />
-    <Explorearround />
+  return (
+    <div>
+<div className="offers">
+  <h1>Offers</h1>
+  <h3>Promotions,Deals & Special offers for you.</h3>
+</div>
+
+<section id="sn-banner" class="section-p1">
+
+
+  <div class="banner-box">
+   <h3>Save 15% with Late Escape Deals</h3>
+   <h4>Check one more</h4>
+    <h4>dedtination off your wishlist </h4>
+    <button class="white"> Explore Deals </button>
   </div>
+
+
+
+  <div class="banner-box banner-box2">
+    <h3>Escape for a while</h3>
+    
+<p>Enjoy the fredom of an extended stay on Booking.com</p>
+    <button class="white">Discover Extended Stays</button>
+  </div>
+</section>
+
+    </div>
+  )
 }
 
 export default Home
