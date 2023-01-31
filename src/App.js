480e89964525586643b8d6a1b624bac084f7bded
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-01-31 18:10:43 +0530

    feat: added routing mechanism

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HotelPage from './pages/hotelList/HotelPage'
 
 import './index.css'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Home />
+            </>
+          }
+        ></Route>
+        <Route path="/stays" element={<HotelPage/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
