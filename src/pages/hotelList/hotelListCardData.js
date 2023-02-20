d069464efc79e645820974650708d553d0f3f3ce
Anand930singh <anand2002king@gmail.com>
2023-02-20 21:32:06 +0530

    dynamic

---
+++ src/pages/hotelList/hotelListCardData.js
@@ -0,0 +1,26 @@
+export const hotelListCardData= [
+    {img:"https://images.pexels.com/photos/1001965/pexels-photo-1001965.jpeg",
+    title:"The Orchid Mumbai Vile Palre",
+     text:"The Orchid - An Ecotel Hotel in Mumbai is Asia's first certified  eco-friendly five-star hotel",
+     price:"1270",
+     location:"1km from Mumbai Airport"
+    },
+    {img:"https://images.pexels.com/photos/161758/governor-s-mansion-montgomery-alabama-grand-staircase-161758.jpeg",
+    title:"Rooms Hotel Kazbegi",
+     text:"The Orchid - An Ecotel Hotel in Mumbai is Asia's first certified  eco-friendly five-star hotel",
+     price:"1110",
+     location:"1km from Mumbai Airport"
+    },
+    {img:"https://images.pexels.com/photos/2869215/pexels-photo-2869215.jpeg",
+    title:"The Orchid Mumbai Vile Palre",
+     text:"The Orchid - An Ecotel Hotel in Mumbai is Asia's first certified  eco-friendly five-star hotel",
+     price:"1380",
+     location:"1km from Mumbai Airport"
+    },
+    {img:"https://images.pexels.com/photos/9959711/pexels-photo-9959711.jpeg",
+    title:"The Orchid Mumbai Vile Palre",
+     text:"The Orchid - An Ecotel Hotel in Mumbai is Asia's first certified  eco-friendly five-star hotel",
+     price:"1270",
+     location:"1km from Mumbai Airport"
+    }
+]
